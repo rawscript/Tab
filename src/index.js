d3013452a95d24fe5d0717e2102f2fa3b8f811f4
Jase Mwaura <rawscript170@gmail.com>
2026-01-05 03:28:18 -0800

    qol upd unverifiedindex modules export

--- src/index.js
+++ src/index.js
@@ -10,13 +10,41 @@ import { NetworkManager } from './utils/NetworkManager';
 import { EncryptionUtils } from './utils/EncryptionUtils';
 import { StorageManager } from './utils/StorageManager';
 
+// New modules
+import { SyncStatusManager } from './sync/SyncStatusManager';
+import { ConflictResolver } from './conflict/ConflictResolver';
+import { KeyManager } from './keys/KeyManager';
+import { MigrationManager } from './migrate/MigrationManager';
+import { ResourceCache } from './cache/ResourceCache';
+import { OfflineQueue } from './queue/OfflineQueue';
+import { AnalyticsManager } from './analytics/AnalyticsManager';
+
+// Enhanced providers
+import { 
+  AWSCognitoAdapter, 
+  OktaAdapter, 
+  AzureADAdapter 
+} from './providers/EnhancedProviders';
+
 export {
   AuthOrchestrator,
   LocalAuth,
   OnlineAuth,
   NetworkManager,
   EncryptionUtils,
-  StorageManager
+  StorageManager,
+  // New modules
+  SyncStatusManager,
+  ConflictResolver,
+  KeyManager,
+  MigrationManager,
+  ResourceCache,
+  OfflineQueue,
+  AnalyticsManager,
+  // Enhanced providers
+  AWSCognitoAdapter,
+  OktaAdapter,
+  AzureADAdapter
 };
 
 // Default export for easy usage
